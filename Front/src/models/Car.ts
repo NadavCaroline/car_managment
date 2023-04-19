02c18b50d68eae704bb8ca7c6a766de8032a9ce3
NadavMassad <massadnadav@gmail.com>
2023-04-19 20:10:17 +0300

    update car model

--- Front/src/models/Car.ts
+++ Front/src/models/Car.ts
@@ -1,15 +1,15 @@
 export default interface CarModel {
     id?: number
-    licenseNum: string
-    nickName:string
-    make: string
-    model: string
-    color: string
-    year:  string
-    garageName:string
-    garagePhone:string
-    department : string
-    dep_name: string
-    image : File | null
-    isDisabled:string
+    licenseNum?: string
+    nickName?:string
+    make?: string
+    model?: string
+    color?: string
+    year?:  string
+    garageName?:string
+    garagePhone?:string
+    department? : string
+    dep_name?: string
+    image ?: File | null
+    isDisabled?:string
 }
