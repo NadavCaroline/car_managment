c59d0827eceb36197f56ae3cc57cb8a685edc02c
NadavMassad <massadnadav@gmail.com>
2023-05-17 15:44:18 +0300

    modal file types

--- Front/src/components/cars/Cars.tsx
+++ Front/src/components/cars/Cars.tsx
@@ -12,13 +12,17 @@ import { depsSelector, getDepsAsync } from '../deps/depsSlicer';
 import { Card, Container, Row, Col, Badge, Button, Modal } from "react-bootstrap";
 import dayjs from 'dayjs';
 import pdfImg from '../../images/pdf.png';
-import { FaUnderline } from 'react-icons/fa';
-import { fontSize } from '@mui/system';
-
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+// import Form from 'react-bootstrap/Form';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 
 
 export function Cars() {
   const [show, setShow] = useState(false);
+  const [showModalFiles, setShowModalFiles] = useState(false);
   const cars = useAppSelector(carsSelector);
   const carMaintenance = useAppSelector(carMaintenanceSelector);
   const fileTypes = useAppSelector(fileTypesSelector);
@@ -45,6 +49,7 @@ export function Cars() {
   const updateCar: CarModel = {}
   const [carMaintenanceArr, setCarMaintenanceArr] = useState<CarMaintenanceModel[]>([]);
   const updateCarMaintenance: CarMaintenanceModel = {}
+  const [isChecked, setIsChecked] = useState(false);
 
   // Handles image upload
   const handleCarImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -67,6 +72,9 @@ export function Cars() {
       //display error
     }
   };
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
   // Pop up for adding car
   const handleExitUpload = () => {
     setaddpopUp(false)
@@ -80,6 +88,18 @@ export function Cars() {
   const handleShow = () => {
     setShow(true);
   };
+  const handleCloseModalFiles = () => {
+    setShowModalFiles(false);
+  };
+
+  const handleShowModalFiles = () => {
+    setShowModalFiles(true);
+  };
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    // Process your form data or perform other actions
+  };
+
   // Handles the update of a car's department
   const handleUpdate = () => {
     updateCar.id = selectedCar?.id
@@ -139,10 +159,21 @@ export function Cars() {
             {cars.map(car =>
               <Col style={{ marginBottom: '10px' }}>
                 <Card id={`divCar-${car.id}`} className='h-100 text-center notSelectedDiv' onClick={(event) => handleCarDivClick(event.currentTarget, String(car.id))}  >
-                  {/* <Card.Img variant="top" src="https://via.placeholder.com/350x150" /> */}
                   <Card.Body >
                     <Card.Title>
-                      {car.nickName} <br /> {car.licenseNum}
+                      <table style={{ width: '100%' }}>
+                        <tr>
+                          <td style={{ width: '20px' }}>
+                            <div data-tooltip={car.isDisabled ? 'לא פעיל' : 'פעיל'} className={`circle ${car.isDisabled ? 'red' : 'green'}`} />
+                          </td>
+                          <td>
+                            {car.nickName} <br /> {car.licenseNum}
+                          </td>
+                          <td style={{ width: '20px' }}>
+                          </td>
+                        </tr>
+                      </table>
+
                     </Card.Title>
                     <Card.Text>
                       <table style={{ margin: '0 auto' }}>
@@ -177,6 +208,7 @@ export function Cars() {
                         מוסך {car.garageName} {car.garagePhone}<br></br>
                       </React.Fragment>
                     )}
+
                     <button className="btn btn-primary" style={{ marginTop: '10px' }} onClick={() => setselectedCar(car)}>עריכה</button>
                   </Card.Body>
                   <Card.Footer>
@@ -221,10 +253,13 @@ export function Cars() {
             )}
           </Row>
         </Container>
-        <form dir="rtl" id="formCarmaintenance" style={{ border: ".2rem solid #ececec", borderRadius: "8px", padding: "1rem" }}>
+        <form dir="rtl" id="formCarmaintenance" onSubmit={handleFormSubmit} style={{ border: ".2rem solid #ececec", borderRadius: "8px", padding: "1rem" }}>
           <div style={{ marginTop: '10px' }}>
             <input placeholder='חיפוש' onChange={(e) => setsearchTerm(e.target.value)} style={{ width: '300px', left: '150px' }} />
             <h3 style={{ color: "rgb(19, 125, 141)", marginRight: "10px", marginBottom: "0px", marginTop: "10px" }} >מסמכי רכב</h3><hr />
+            <div style={{ textAlign: 'left' }}>
+              <button style={{ marginLeft: "10px", marginBottom: "10px" }} className="btn btn-primary" onClick={() => {/*setaddpopUp(true);*/handleShowModalFiles() }}>הוספת מסמך</button>
+            </div>
             <Container>
               <Row className="align-items-stretch" xs={1} md={2} lg={3} >
                 {selectedCarId && carMaintenance && carMaintenance.filter(carMaintenance => (carMaintenance.car == selectedCarId && (carMaintenance.car_file_type_name?.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
@@ -263,8 +298,114 @@ export function Cars() {
           </div >
 
         </form>
-       
 
+        {/* modal for file type car */}
+        <Modal show={showModalFiles} onHide={handleCloseModalFiles} style={{ direction: 'rtl' }}>
+          <Modal.Header style={{ backgroundColor: "rgb(19, 125, 141)" }}  >
+            <Modal.Title style={{ color: "white" }}>הוספת מסמך לרכב</Modal.Title>
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              style={{ color: 'white' }}
+              onClick={handleCloseModalFiles}
+            ></button>
+          </Modal.Header>
+          <Modal.Body>
+            <table>
+              <tbody>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>סוג מסמך</td>
+                  <td>
+                    <select value={department} onChange={(e) => setdepartment(e.target.value)}>
+                      <option value="" disabled={true}>בחר מחלקה </option>
+                      {fileTypes.map((filetype) => (
+                        <option key={filetype.id} value={filetype.id}>
+                          {filetype.name}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>תאריך טיפול</td>
+                  <td>
+                    <div >
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DatePicker', 'MobileTimePicker']}>
+                          {/* Date Picking Component */}
+                          <DemoItem >
+                            <DatePicker
+                              minDate={dayjs()}
+                              format='DD-MM-YYYY'
+                            // value={selectedStartDate}
+                            // onChange={handleStartDateChange}
+                            />
+                          </DemoItem>
+                        </DemoContainer>
+                      </LocalizationProvider>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>תאריך פג תוקף</td>
+                  <td>
+                    <div >
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DatePicker', 'MobileTimePicker']}>
+                          {/* Date Picking Component */}
+                          <DemoItem >
+                            <DatePicker 
+                              minDate={dayjs()}
+                              format='DD-MM-YYYY'
+                            // value={selectedStartDate}
+                            // onChange={handleStartDateChange}
+                            />
+                          </DemoItem>
+                        </DemoContainer>
+                      </LocalizationProvider>
+                    </div>
+                  </td>
+                </tr>
+                <tr >
+                  <td style={{ paddingLeft: '10px' }}>מסמך</td>
+                  <td>
+                    <input required type="file" />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>
+                    ק"מ טיפול הבא
+                  </td>
+                  <td style={{ paddingLeft: '10px' }}>
+                    <input />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>
+                    הערות
+                  </td>
+                  <td>
+                    <div className="form-floating mb-2">
+                      <textarea style={{ width: '100%' }} id="filecomment" rows={3} placeholder="הערות"></textarea>
+                    </div>
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+
+          </Modal.Body>
+          <Modal.Footer>
+            {/* <Button variant="secondary" onClick={handleClose} >
+              Close
+            </Button> */}
+            <Button className="btn btn-primary" onClick={() => handlePostRequest()}>שמור</Button>
+            {/* Additional buttons or content */}
+          </Modal.Footer>
+        </Modal>
+
+        {/* Modal for cars */}
         <Modal show={show} onHide={handleClose} style={{ direction: 'rtl' }}>
           <Modal.Header style={{ backgroundColor: "rgb(19, 125, 141)" }}  >
             <Modal.Title style={{ color: "white" }}>הוספת רכב</Modal.Title>
@@ -277,64 +418,106 @@ export function Cars() {
             ></button>
           </Modal.Header>
           <Modal.Body>
-            <div>
-              כינוי רכב:
-              <input onChange={(e) => setNickName(e.target.value)} />
-            </div>
-            <div>
-              מספר לוחית רישוי:
-              <input required onChange={(e) => setlicenseNum(e.target.value)} />
-            </div>
-            <div>
-              יצרן:
-              <input required onChange={(e) => setmake(e.target.value)} />
-            </div>
-            <div>
-              דגם:
-              <input required onChange={(e) => setmodel(e.target.value)} />
-            </div>
-            <div>
-              צבע:
-              <input required onChange={(e) => setcolor(e.target.value)} />
-            </div>
-            <div>
-              שנה:
-              <input required onChange={(e) => setyear(e.target.value)} />
-            </div>
-            <div>
-              מחלקה:
-              <select value={department} onChange={(e) => setdepartment(e.target.value)}>
-                <option value="" disabled={true}>בחר מחלקה חדשה</option>
-                {departments.map((dep) => (
-                  <option key={dep.id} value={dep.id}>
-                    {dep.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              שם מוסך :
-              <input onChange={(e) => setGarageName(e.target.value)} />
-            </div>
-            <div>
-              טלפון מוסך :
-              <input onChange={(e) => setGaragePhone(e.target.value)} />
-            </div>
-            <div>
-              תמונת הרכב:
-              <input required type='file' onChange={handleCarImageChange} />
-              {carImage &&
-                <div>
-                  <img src={URL.createObjectURL(carImage)}
-                    alt={carImage.name}
-                    style={{ width: '150px', height: '100px' }} /><br />
-                </div>}
-            </div>
-            {fileTypes.map((fileType, index) => (
-              <div>{fileType.name}
-                <input required type='file' onChange={(e) => handleFileTypesChange(String(fileType.id), e)} />
-              </div>
-            ))}
+            <table>
+              <tbody>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>כינוי רכב</td>
+                  <td>
+                    <input onChange={(e) => setNickName(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>מספר רישוי</td>
+                  <td>
+                    <input required onChange={(e) => setlicenseNum(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>יצרן</td>
+                  <td>
+                    <input required onChange={(e) => setmake(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>דגם</td>
+                  <td>
+                    <input required onChange={(e) => setmodel(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>צבע</td>
+                  <td>
+                    <input required onChange={(e) => setcolor(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>שנה</td>
+                  <td>
+                    <input required onChange={(e) => setyear(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>מחלקה</td>
+                  <td>
+                    <select value={department} onChange={(e) => setdepartment(e.target.value)}>
+                      <option value="" disabled={true}>בחר מחלקה </option>
+                      {departments.map((dep) => (
+                        <option key={dep.id} value={dep.id}>
+                          {dep.name}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>שם מוסך</td>
+                  <td>
+                    <input onChange={(e) => setGarageName(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>טלפון מוסך</td>
+                  <td>
+                    <input onChange={(e) => setGaragePhone(e.target.value)} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>תמונת הרכב</td>
+                  <td>
+                    <input required type="file" onChange={handleCarImageChange} />
+                    {carImage && (
+                      <div>
+                        <img
+                          src={URL.createObjectURL(carImage)}
+                          alt={carImage.name}
+                          style={{ width: '150px', height: '100px' }}
+                        /><br />
+                      </div>
+                    )}
+
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingLeft: '10px' }}>פעיל</td>
+                  <td>
+                    <BootstrapSwitchButton
+                      checked={true}
+                      onlabel='כן'
+                      offlabel='לא'
+                      offstyle="danger"
+                      onstyle="success"
+                      size='xs'
+                      // onChange={(checked: boolean) => {
+                      //   this.setState({ isUserAdmin: checked })
+                      // }}
+                    />
+                   
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+
           </Modal.Body>
           <Modal.Footer>
             {/* <Button variant="secondary" onClick={handleClose} >
@@ -420,7 +603,7 @@ export function Cars() {
               <div>
                 שינוי מחלקה:
                 <select value={newDep} onChange={(e) => setnewDep(e.target.value)}>
-                  <option value="" disabled={true}>בחר מחלקה חדשה</option>
+                  <option value="" disabled={true}>בחר מחלקה </option>
                   {departments.map((dep) => (
                     <option key={dep.id} value={dep.id}>
                       {dep.name}
